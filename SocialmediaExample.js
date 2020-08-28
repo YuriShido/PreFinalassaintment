ab4b09fd165020b93dc16071d1709ed1f1369fdf
YuriShido <sydonce57@gmail.com>
2020-08-28 15:25:27 -0700

    after828

--- SocialmediaExample.js
+++ SocialmediaExample.js
@@ -33,20 +33,66 @@ let users = {
   },
 
 
+  generateUid: function() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  },
+  
   getUserByID: function(userID) {
     // should return a sentence containing all of the info about the user (name age, and bio)
+  //  console.log(users[userID])
+  //  console.log(`Hi my name is ${users[userID].name}.  I'm ${users[userID].age} years old. ${users[userID].bio}` );
+   return `Hi my name is ${users[userID].name}.  I'm ${users[userID].age} years old. ${users[userID].bio}` ;
   },
 
   getUserByName: function(userName) {
-     // should return a sentence containing all of the info about the user (name age and bio)
+        // should return a sentence containing all of the info about the user (name age and bio)
+    let userID;
+    for (let user in users) {
+      if(users[user].name === userName)
+      userID = users[user].id
+    } 
+    console.log(`Hi my name is ${users[userID].name}.  I'm ${users[userID].age} years old. ${users[userID].bio}` );
+    // console.log(users[userID].name, users[userID].age, users[userID].bio)
+    //  let userID = Object.keys(users)
+    //  console.log(userID)
+    //  let Names = Object.values(users)
+    //  let det = Object.values(users[userID[userName]])
+    //  let Names = users[userID[0]].name
+    //  console.log(Names)
+  
   },
 
-  addUser: function(name, price, desc) {
+  addUser: function(name, age, bio, friends) {
+   
+    
+    let newUser = {
+      id: users.generateUid(),
+      name: name,
+      age: age,
+      bio: bio,
+      friends: friends
+    };
+     users[newUser.id] = newUser
+     console.log(users[newUser.id]);
+
     //will need a random id as well when creating a new user. console log the user object to confirm it was added. 
   },
 
   getFriendsByID: function(userID) {
     // this function will take in a userID, and using this, return a list of all of their friends names. 
+
+    let FriendsID = users[userID].friends
+    for(i = 0; i < userID.length; i++) {
+      
+      console.log(users[FriendsID[i]].name);
+    } 
+    
   }
 
 }
+
+// users.getUserByID('p01')
+// users.getUserByName('Taylor Swift')
+users.addUser('Beyonce', '34', 'She is Diva', 'p01')
+
+// users.getFriendsByID('p03')
